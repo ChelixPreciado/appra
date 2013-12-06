711e2edeabaacf8aa0f2b7ea4450798e896b769b
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2013-12-06 03:00:00 -0600

    zoom > 14 draw

--- maps/geojson/draw.js
+++ maps/geojson/draw.js
@@ -29,38 +29,43 @@ $(document).ready( function () {
 	map.addControl(drawControl);
 
 	map.on('draw:created', function (e) {
+		var zoom = map._zoom;
 		
-		var type = e.layerType,
-			layer = e.layer;
+		if(zoom > 14) {
+			var type = e.layerType,
+				layer = e.layer;
 
-		if (type === 'marker') {
-			layer.bindPopup('A popup!');
-		}
-		
-		removeLayers();
-		drawnItems.addLayer(layer);
-		
-		var geoJSON = layer.toGeoJSON();
-		
-		/*each layers show*/
-		var layers = "";
-		$.each($(".l-show"), function(key, value) { layers = layers + $(this).attr("id") + ","; });
-		layers = layers.replace(/,+$/,'');
-		
-		$.ajax({
-			cache: false,
-			type: 'POST',              
-			url: '/appra/index.php/api-draw/' + layers,
-			dataType: 'json',
-			data: geoJSON,
-			success: function (d) {
-				printResults(d);
-				
-				$(".loading").hide();
-			},
-			error: function (response) {
-				$(".loading").hide();
+			if (type === 'marker') {
+				layer.bindPopup('A popup!');
 			}
-		});
+			
+			removeLayers();
+			drawnItems.addLayer(layer);
+			
+			var geoJSON = layer.toGeoJSON();
+			
+			/*each layers show*/
+			var layers = "";
+			$.each($(".l-show"), function(key, value) { layers = layers + $(this).attr("id") + ","; });
+			layers = layers.replace(/,+$/,'');
+			
+			$.ajax({
+				cache: false,
+				type: 'POST',              
+				url: '/appra/index.php/api-draw/' + layers,
+				dataType: 'json',
+				data: geoJSON,
+				success: function (d) {
+					printResults(d);
+					
+					$(".loading").hide();
+				},
+				error: function (response) {
+					$(".loading").hide();
+				}
+			});
+		} else {
+			$(".loading").hide();
+		}
 	});
 });
